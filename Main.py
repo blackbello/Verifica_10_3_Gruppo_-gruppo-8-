3768fa8c64912f52bd833051ff12cf9a458928e6
blackbello <hocine.lazeb@polocodigoro.edu.it>
2023-03-10 13:09:53 +0100

    capo gruppo

---
+++ Main.py
@@ -0,0 +1,6 @@
+import saluto_lazeb, saluto_carli.r, saluto_rossi, saluto_olivo
+def saluto_Lazeb_Gruppo():
+    saluto= "Saluto da hocine lazeb dal gruppo 8"
+    print(saluto)
+
+saluto_Lazeb_Gruppo()
