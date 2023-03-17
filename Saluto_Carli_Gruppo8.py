55cb06f9d952fe88eedd1d521a1f4601ecf75b77
blackbello <120778222+blackbello@users.noreply.github.com>
2023-03-17 12:19:22 +0100

    Delete Saluto_Carli_Gruppo8.py

--- Saluto_Carli_Gruppo8.py
+++
@@ -1,8 +0,0 @@
-def saluto_nome_cognome(nome, cognome):
-    saluto = "Ciao, " + nome + " " + cognome + "!"
-    return saluto
-
-nome = input("Inserisci il tuo nome: ")
-cognome = input("Inserisci il tuo cognome: ")
-saluto = saluto_nome_cognome(nome, cognome)
-print(saluto)
